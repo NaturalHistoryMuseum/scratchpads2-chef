f35baafe59aef1480f4fc0feea897441a81b5b3a
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-05-14 10:10:46 +0100

    Updated the control default recipe to setup NFS

--- cookbooks/control/recipes/default.rb
+++ cookbooks/control/recipes/default.rb
@@ -5,4 +5,13 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 include_recipe 'apt::default'
+
 include_recipe 'apache2::default'
+
+include_recipe 'nfs::server'
+nfs_export "/var/aegir/platforms" do
+  network ['sp-app-1','sp-app-2', 'sp-app-3', 'sp-app-4']
+  writeable true
+  sync true
+  options ['no_subtree_check']
+end

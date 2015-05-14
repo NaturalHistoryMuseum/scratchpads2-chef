2d3ff5371079ed36ff0a304c4a22cf2e96263e6f
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-05-14 16:42:33 +0100

    Added PHP, Apache and Percona.

--- cookbooks/control/recipes/default.rb
+++ cookbooks/control/recipes/default.rb
@@ -4,9 +4,16 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-include_recipe 'apt::default'
+include_recipe 'apt'
 
-include_recipe 'apache2::default'
+include_recipe 'php'
+php_pear_channel "pear.drush.org" do
+  action :discover
+end
+
+include_recipe 'apache2'
+include_recipe 'apache2::mpm_prefork'
+include_recipe 'apache2::mod_php5'
 
 include_recipe 'nfs::server'
 nfs_export "/var/aegir/platforms" do
@@ -16,4 +23,15 @@ nfs_export "/var/aegir/platforms" do
   options ['no_subtree_check']
 end
 
-#package ['php5','php5-cli','php5-curl','php5-gd','php5-mysql','php5-memcache']
+# Manually add the Percona apt repository, as we need to use
+# the wheezy repo, and not the jessie one (which isn't yet
+# complete).
+apt_repository "percona" do
+  uri node["percona"]["apt_uri"]
+  distribution "wheezy"
+  components ["main"]
+  keyserver node["percona"]["apt_keyserver"]
+  key node["percona"]["apt_key"]
+end
+include_recipe 'percona::client'
+include_recipe 'percona::server'

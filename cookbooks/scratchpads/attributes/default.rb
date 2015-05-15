cc910e9f368639e542c5e01444fb5107ecc39ccb
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-05-15 12:25:27 +0100

    Added attributes and reference them.

--- cookbooks/scratchpads/attributes/default.rb
+++ cookbooks/scratchpads/attributes/default.rb
@@ -1,1 +1,4 @@
-default["encrypted_data_bag"] = "passwords"
+default["scratchpads"]["percona"]["encrypted_data_bag"] = "passwords"
+default["scratchpads"]["percona"]["host"] = "127.0.0.1"
+default["scratchpads"]["percona"]["username"] = "root"
+default["scratchpads"]["percona"]["percona-functions-file"] = "/tmp/percona-functions.sql"
--- cookbooks/scratchpads/recipes/percona-functions.rb
+++ cookbooks/scratchpads/recipes/percona-functions.rb
@@ -5,7 +5,7 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 # Copy the percona-functions SQL file.
-cookbook_file '/tmp/percona-functions.sql' do
+cookbook_file node['scratchpads']['percona']['percona-functions-file'] do
   source 'percona-functions.sql'
   owner 'root'
   group 'root'
@@ -13,8 +13,8 @@ cookbook_file '/tmp/percona-functions.sql' do
 end
 
 # Execute the MySQL using the password set in the Percona passwords bag.
-passwords = EncryptedPasswords.new(node, node["encrypted_data_bag"])
+passwords = EncryptedPasswords.new(node, node["scratchpads"]["percona"]["encrypted_data_bag"])
 execute 'percona functions' do
   root_pw = passwords.root_password
-  command "mysql -h #{node['scratchpads']['percona']['host']} -u #{node['scratchpads']['percona']['username']} -p'#{roow_pw}' < #{node['scratchpads']['percona']['percona-functions-file']}"
+#  command "mysql -h #{node['scratchpads']['percona']['host']} -u #{node['scratchpads']['percona']['username']} -p'#{roow_pw}' < #{node['scratchpads']['percona']['percona-functions-file']}"
 end

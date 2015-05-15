7cd6fd5ef4b1ec9def95aa2896e0a7340e00422c
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-05-15 16:27:39 +0100

    Delete all "local" users except for the "localhost" one.

--- cookbooks/scratchpads/files/default/secure-installation.sql
+++ cookbooks/scratchpads/files/default/secure-installation.sql
@@ -1,7 +1,7 @@
 -- remove anonymous users
 DELETE FROM mysql.user WHERE User='';
 -- Disallow root login remotely
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost');
 -- Remove test database and access to it
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';

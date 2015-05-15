fd6ba3e12570823ba6d05cd14dc2472a06ee6677
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-05-15 16:28:06 +0100

    Added attributes for the control recipes.

---
+++ cookbooks/control/attributes/default.rb
@@ -0,0 +1,5 @@
+default["control"]["percona"]["encrypted_data_bag"] = "passwords"
+default["control"]["database"]["aegir"] = "aegir"
+default["control"]["database"]["host"] = "localhost"
+default["control"]["database"]["username"] = "root"
+default["control"]["database"]["dbname"] = "aegir"

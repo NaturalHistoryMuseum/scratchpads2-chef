d8b2ed595d68e1faf9e7bdadb28678fc9d252522
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-05-18 15:16:42 +0100

    Attempted to clean up the attributes.

--- cookbooks/scratchpads/attributes/default.rb
+++ cookbooks/scratchpads/attributes/default.rb
@@ -1,8 +1,19 @@
-default["scratchpads"]["percona"]["encrypted_data_bag"] = "passwords"
-default["scratchpads"]["percona"]["host"] = "localhost"
-default["scratchpads"]["percona"]["username"] = "root"
+# Passwords bag
+default["scratchpads"]["encrypted_data_bag"] = "passwords"
+
+# FQDN of the control server (this probably shouldn't be set here)
+default["scratchpads"]["control"]["fqdn"] = "sp-control-1.nhm.ac.uk"
+
+# Aegir database user
+default["scratchpads"]["control"]["aegir"]["dbuser"] = "aegir"
+default["scratchpads"]["control"]["aegir"]["dbuserhost"] = "%"
+#default["scratchpads"]["control"]["aegir"]["host"] = "sp-control-1"
+default["scratchpads"]["control"]["aegir"]["dbname"] = "aegir"
+
+# Aegir database server
+default["scratchpads"]["control"]["dbserver"] = "localhost"
+default["scratchpads"]["control"]["dbuser"] = "root"
+
+# Extra SQL files
 default["scratchpads"]["percona"]["percona-functions-file"] = "/tmp/percona-functions.sql"
 default["scratchpads"]["percona"]["secure-installation-file"] = "/tmp/secure-installation.sql"
-default["scratchpads"]["control"]["database"]["dbuser"] = "aegir"
-default["scratchpads"]["control"]["database"]["host"] = "sp-control-1"
-default["scratchpads"]["control"]["database"]["dbname"] = "aegir"

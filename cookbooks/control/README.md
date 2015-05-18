927a5d571126935e2b6ae92523a762bd673cd13b
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-05-18 11:15:08 +0100

    Removed the control cookbook and moved the recipe into the scratchpads cookbook (where all custom recipes will go).

--- cookbooks/control/README.md
+++
@@ -1,4 +0,0 @@
-# control
-
-TODO: Enter the cookbook description here.
-
--- cookbooks/control/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-default["control"]["percona"]["encrypted_data_bag"] = "passwords"
-default["control"]["database"]["aegir"] = "aegir"
-default["control"]["database"]["host"] = "localhost"
-default["control"]["database"]["username"] = "root"
-default["control"]["database"]["dbname"] = "aegir"

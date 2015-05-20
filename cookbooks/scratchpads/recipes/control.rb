fed1e301eb4238b45fe5c43b8ae1d99f5adf3bad
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-05-20 10:26:57 +0100

    Removes the 127.0.1.1 entry from the /etc/hosts file which is causing issues for Varnish (and probably other services).

--- cookbooks/scratchpads/recipes/control.rb
+++ cookbooks/scratchpads/recipes/control.rb
@@ -4,6 +4,11 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
+# First we remove the 127.0.1.1 entry from the hosts file
+hostsfile_entry '127.0.1.1' do
+  action    :remove
+end
+
 # Update apt repository and update
 include_recipe 'apt'
 

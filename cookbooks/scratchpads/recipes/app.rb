1ecabb2f49d330cabaa40686a1c882e057cbc8f3
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-05-20 17:12:27 +0100

    Added the start of the app recipe.

--- cookbooks/scratchpads/recipes/app.rb
+++ cookbooks/scratchpads/recipes/app.rb
@@ -3,3 +3,18 @@
 # Recipe:: app
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+# Mount the folder from the control server
+include_recipe 'nfs'
+if Chef::Config[:solo]
+  control_hosts = {"sp-control-1" => {"fqdn" => "sp-control-1"}}
+else
+  control_hosts = search(:node, "role:control")
+end
+control_host = control_hosts.first
+mount "/var/aegir/platforms" do
+  device "#{control_host['fqdn']}:/var/aegir/platforms"
+  fstype "nfs"
+  options "rw,bg,ac,noatime"
+  action [:mount, :enable]
+end

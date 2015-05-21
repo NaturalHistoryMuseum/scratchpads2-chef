ab578c9108d29230e37fb075ba7c4c22ee675ae1
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-05-21 12:21:39 +0100

    Added additional attributes.

--- cookbooks/scratchpads/attributes/default.rb
+++ cookbooks/scratchpads/attributes/default.rb
@@ -18,3 +18,18 @@ default['scratchpads']['control']['admin_email'] = "s.rycroft@nhm.ac.uk"
 # Extra SQL files
 default["scratchpads"]["percona"]["percona-functions-file"] = "/tmp/percona-functions.sql"
 default["scratchpads"]["percona"]["secure-installation-file"] = "/tmp/secure-installation.sql"
+
+# Aegir settings
+default["scratchpads"]["aegir"]["home_folder"] = "/var/aegir"
+default["scratchpads"]["aegir"]["hostmaster_folder"] = "hostmaster"
+default["scratchpads"]["aegir"]["environment"] = ({
+  'SHELL' => '/bin/bash',
+  'TERM' => 'xterm',
+  'USER' => 'aegir',
+  'MAIL' => '/var/mail/aegir',
+  'PATH' => '/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games',
+  'PWD' => default["scratchpads"]["aegir"]["home_folder"],
+  'LANG' => 'en_GB.UTF-8',
+  'SHLVL' => '1',
+  'HOME' => default["scratchpads"]["aegir"]["home_folder"]
+})

2ee5748c6678d0aced3e288dfdf9916ca8006f2b
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-05-21 14:25:53 +0100

    Moved creation of aegir user to aegir recipe.

--- cookbooks/scratchpads/recipes/default.rb
+++ cookbooks/scratchpads/recipes/default.rb
@@ -12,23 +12,5 @@ end
 # Update apt repository and update
 include_recipe 'apt'
 
-# Create the aegir user
-user 'aegir' do
-  group 'www-data'
-  system true
-  shell '/bin/bash'
-  comment 'User which runs all of the behind the scenes actions.'
-  home '/var/aegir'
-  manage_home
-end
-# Add the aegir user to sudoers and ensure it does not need a password.
-sudo 'aegir' do
-  user 'aegir'
-  nopasswd true
-end
-
 # Install Gkrellmd on all servers
 include_recipe 'gkrellmd'
-
-# Only include the postfix recipe IF we are the control server
-include_recipe 'postfix'

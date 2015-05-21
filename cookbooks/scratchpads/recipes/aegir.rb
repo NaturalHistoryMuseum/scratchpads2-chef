d3ca6a4594c4e0a793ccd21b75690d819d9895e4
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-05-21 10:55:18 +0100

    Added bash code which needs converting to install various modules and options on the hostmaster. Created a template for creating the hosting-queued.service file.

--- cookbooks/scratchpads/recipes/aegir.rb
+++ cookbooks/scratchpads/recipes/aegir.rb
@@ -66,6 +66,42 @@ if node.automatic.roles.index("control") then
       'HOME' => '/var/aegir'
     )
   end
+# # Download the memcache module, and move it to the correct folder (under 
+# # contrib)
+# su -l -s /bin/bash -c "drush @hm dl memcache varnish" aegir
+# mkdir `ls -d1 /var/aegir/hostmaster*`/sites/all/modules/contrib
+# mv `ls -d1 /var/aegir/hostmaster*`/sites/all/modules/memcache `ls -d1 /var/aegir/hostmaster*`/sites/all/modules/varnish `ls -d1 /var/aegir/hostmaster*`/sites/all/modules/contrib
+# # Install additional modules
+# su -l -s /bin/bash -c "drush @hm en hosting_queued hosting_alias hosting_clone hosting_cron hosting_migrate hosting_signup hosting_task_gc hosting_web_pack -y" aegir
+# # Set the password for the admin user to "password", just to make it easier to
+# # access and reset
+# su -l -s /bin/bash -c "drush @hm upwd admin --password=scratchpads -y" aegir
+# # Update the cron for the aegir user
+# # su -l -s /bin/bash -c 'echo "NEW CRON LINE"|crontab -' aegir 
+# # Generate SSH keys
+# su -l -s /bin/bash -c "ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa" aegir
+# # COPY THE key to the webserver, and download it from there on to each host we
+# # setup
+# cp /var/aegir/.ssh/id_rsa.pub `ls -d1 /var/aegir/hostmaster*`
+# echo "Host *
+#    StrictHostKeyChecking no
+#    UserKnownHostsFile=/dev/null" > ~aegir/.ssh/config
+# chown aegir:aegir ~aegir/.ssh/config
+
+  template '/etc/systemd/system/hosting-queued.service' do
+    path '/etc/systemd/system/hosting-queued.service'
+    source 'hosting-queued.service.erb'
+    cookbook 'scratchpads'
+    owner 'root'
+    group 'root'
+    mode '0644'
+    action :create
+    notifies :run, 'execute[restart_systemctl_daemon]', :immediately
+    notifies :restart, 'service[hosting-queued]', :delayed
+  end
+  system "hosting-queued" do
+    action :enable,:restart
+  end
 end
 
 # Link the aegir configuration to the apache sites folder

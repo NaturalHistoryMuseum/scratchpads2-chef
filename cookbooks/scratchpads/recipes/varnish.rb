5bd67cbe747d8de7b0b8c6083f3126c680bc8c1e
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-05-21 13:24:20 +0100

    Populate the Varnish secret file from the encrypted data bag.

--- cookbooks/scratchpads/recipes/varnish.rb
+++ cookbooks/scratchpads/recipes/varnish.rb
@@ -52,3 +52,10 @@ template '/etc/default/varnish' do
   action :create
   notifies :restart, 'service[varnish]', :delayed
 end
+passwords = EncryptedPasswords.new(node, node["scratchpads"]["encrypted_data_bag"])
+varnish_secret = passwords.find_password "varnish", "secret"
+execute 'update varnish secret file' do
+  command "echo \"#{varnish_secret}\" > #{node['varnish']['secret_file']}"
+  group 'root'
+  user 'root'
+end

fc0090602d103fa474ea381334ff061a99463e57
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-05-21 14:14:04 +0100

    Automatically configure Postfix on the servers.

---
+++ cookbooks/scratchpads/recipes/postfix.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: scratchpads
+# Recipe:: postfix
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+
+# Only include the postfix recipe IF we are the control server
+if node.automatic.roles.index("control") then
+  include_recipe 'postfix'
+else
+
+end

5acce82bd7d81c9ae369adbea5b243c53ac055d8
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-05-26 13:46:27 +0100

    Added all-aegir-domains bash script.

--- cookbooks/scratchpads/recipes/postfix.rb
+++ cookbooks/scratchpads/recipes/postfix.rb
@@ -6,6 +6,15 @@
 
 # Only include the postfix recipe IF we are the control server
 if node.automatic.roles.index("control") then
+  template "/usr/local/bin/all-aegir-domains" do
+    path "/usr/local/bin/all-aegir-domains"
+    source 'all-aegir-domains.erb'
+    cookbook 'scratchpads'
+    owner "root"
+    group "root"
+    mode '0755'
+    action :create
+  end
   include_recipe 'postfix::server'
 else
   include_recipe 'postfix::client'

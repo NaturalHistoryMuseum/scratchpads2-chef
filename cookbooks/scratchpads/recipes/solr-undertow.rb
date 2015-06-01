7315639db1ab8765c0b2b084db9a5d20a9ce188a
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-06-01 15:07:34 +0100

    Added java cookbook. Added java to the search role. Tweaks to the solr-undertow recipe.

--- cookbooks/scratchpads/recipes/solr-undertow.rb
+++ cookbooks/scratchpads/recipes/solr-undertow.rb
@@ -16,21 +16,80 @@ user node['scratchpads']['solr-undertow']['user'] do
   manage_home
 end
 
-# Check out the solr-undertow repository
-git "#{node['scratchpads']['solr-undertow']['application_folder']}" do
-  repository node['scratchpads']['solr-undertow']['repository']
+# Create the folder to clone into
+directory node['scratchpads']['solr-undertow']['application_folder'] do
+  owner node['scratchpads']['solr-undertow']['user']
   group node['scratchpads']['solr-undertow']['group']
+  mode 0755
+  action :create
+end 
+directory node['scratchpads']['solr-undertow']['data_folder'] do
+  owner node['scratchpads']['solr-undertow']['user']
+  group node['scratchpads']['solr-undertow']['group']
+  mode 0755
+  action :create
+end
+directory "#{node['scratchpads']['solr-undertow']['data_folder']}/#{node['scratchpads']['solr-undertow']['solr_home_folder']}" do
+  owner node['scratchpads']['solr-undertow']['user']
+  group node['scratchpads']['solr-undertow']['group']
+  mode 0755
+  action :create
+end
+directory "#{node['scratchpads']['solr-undertow']['solr_logs_folder']}" do
+  owner node['scratchpads']['solr-undertow']['user']
+  group node['scratchpads']['solr-undertow']['group']
+  mode 0755
+  action :create
+end
+
+# Download the latest release.
+unless(::File.exists?("#{node['scratchpads']['solr-undertow']['application_folder']}/#{node['scratchpads']['solr-undertow']['release_version']}-#{node['scratchpads']['solr-undertow']['solr_version']}.tar.gz"))
+  remote_file "#{node['scratchpads']['solr-undertow']['application_folder']}/#{node['scratchpads']['solr-undertow']['release_version']}-#{node['scratchpads']['solr-undertow']['solr_version']}.tar.gz" do
+    action :create
+    source node['scratchpads']['solr-undertow']['release_url']
+    owner node['scratchpads']['solr-undertow']['user']
+    group node['scratchpads']['solr-undertow']['group']
+    mode 0444
+  end
+
+  # Extract the shit out of that release
+  execute 'extract the solr-undertow release' do
+    cwd node['scratchpads']['solr-undertow']['application_folder']
+    command "tar xfz #{node['scratchpads']['solr-undertow']['release_version']}-#{node['scratchpads']['solr-undertow']['solr_version']}.tar.gz --strip-components 1"
+    user node['scratchpads']['solr-undertow']['user']
+    group node['scratchpads']['solr-undertow']['group']
+    not_if { ::File.exists?("#{node['scratchpads']['solr-undertow']['application_folder']}/bin")}
+  end
+end
+
+# Create the configuration file that is used to start solr-undertow
+template node['scratchpads']['solr-undertow']['conf_file']['path'] do
+  path node['scratchpads']['solr-undertow']['conf_file']['path']
+  source node['scratchpads']['solr-undertow']['conf_file']['source']
+  cookbook node['scratchpads']['solr-undertow']['conf_file']['cookbook']
+  owner node['scratchpads']['solr-undertow']['conf_file']['owner']
+  group node['scratchpads']['solr-undertow']['conf_file']['group']
+  mode node['scratchpads']['solr-undertow']['conf_file']['mode']
+  action :create
+end
+
+# Create a symlink to the wars folder
+execute 'link to wars folder' do
+  cwd node['scratchpads']['solr-undertow']['data_folder']
+  command "ln -s #{node['scratchpads']['solr-undertow']['application_folder']}/example/solr-wars"
   user node['scratchpads']['solr-undertow']['user']
+  group node['scratchpads']['solr-undertow']['group']
+  not_if { ::File.exists?("#{node['scratchpads']['solr-undertow']['data_folder']}/solr-wars")}
 end
 
 # Create the bash script that starts the server
-template "#{node['scratchpads']['solr-undertow']['bash_script']}" do
-  path "#{node['scratchpads']['solr-undertow']['bash_script']}"
-  source 'solr-undertow.bash.erb'
-  cookbook 'scratchpads'
-  owner 'root'
-  group 'root'
-  mode '0755'
+template node['scratchpads']['solr-undertow']['bash_script']['path'] do
+  path node['scratchpads']['solr-undertow']['bash_script']['path']
+  source node['scratchpads']['solr-undertow']['bash_script']['source']
+  cookbook node['scratchpads']['solr-undertow']['bash_script']['cookbook']
+  owner node['scratchpads']['solr-undertow']['bash_script']['owner']
+  group node['scratchpads']['solr-undertow']['bash_script']['group']
+  mode node['scratchpads']['solr-undertow']['bash_script']['mode']
   action :create
 end
 
@@ -39,14 +98,19 @@ execute 'restart_systemctl_daemon' do
   command 'systemctl daemon-reload'
   action :nothing
 end
-template "#{node['scratchpads']['solr-undertow']['service_path']}" do
-  path "#{node['scratchpads']['solr-undertow']['service_path']}"
-  source "#{node['scratchpads']['solr-undertow']['service_template']}"
-  cookbook "#{node['scratchpads']['solr-undertow']['service_template_cookbook']}"
-  owner 'root'
-  group 'root'
-  mode '0644'
+template node['scratchpads']['solr-undertow']['systemd_service']['path'] do
+  path node['scratchpads']['solr-undertow']['systemd_service']['path']
+  source node['scratchpads']['solr-undertow']['systemd_service']['source']
+  cookbook node['scratchpads']['solr-undertow']['systemd_service']['cookbook']
+  owner node['scratchpads']['solr-undertow']['systemd_service']['owner']
+  group node['scratchpads']['solr-undertow']['systemd_service']['group']
+  mode node['scratchpads']['solr-undertow']['systemd_service']['mode']
   action :create
   notifies :run, 'execute[restart_systemctl_daemon]', :immediately
-  notifies :restart, 'service[solr-undertow]', :delayed
+end
+
+# Start the service
+service 'solr-undertow' do
+  action [:enable, :start]
+  provider Chef::Provider::Service::Systemd
 end

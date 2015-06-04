9843439fdb77d313a72aed91765f27213bb674c1
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-06-04 17:05:56 +0100

    Try to make the attributes file easier to read by grouping the templates.

--- cookbooks/scratchpads/attributes/solr-undertow.rb
+++ cookbooks/scratchpads/attributes/solr-undertow.rb
@@ -18,51 +18,65 @@ default['scratchpads']['solr-undertow']['memory']['maxpermsize'] = '512m'
 default['scratchpads']['solr-undertow']['memory']['permsize'] = '256m'
 default['scratchpads']['solr-undertow']['options'] = "-Xms#{node['scratchpads']['solr-undertow']['memory']['xms']} -Xmx#{node['scratchpads']['solr-undertow']['memory']['xmx']} -XX:MaxPermSize=#{node['scratchpads']['solr-undertow']['memory']['maxpermsize']} -XX:PermSize=#{node['scratchpads']['solr-undertow']['memory']['permsize']}"
 # Init script
-default['scratchpads']['solr-undertow']['templates']['bash_script']['path'] = '/usr/local/sbin/solr-undertow'
-default['scratchpads']['solr-undertow']['templates']['bash_script']['cookbook'] = 'scratchpads'
-default['scratchpads']['solr-undertow']['templates']['bash_script']['source'] = 'solr-undertow.bash.erb'
-default['scratchpads']['solr-undertow']['templates']['bash_script']['owner'] = 'root'
-default['scratchpads']['solr-undertow']['templates']['bash_script']['group'] = 'root'
-default['scratchpads']['solr-undertow']['templates']['bash_script']['mode'] = '0755'
+default['scratchpads']['solr-undertow']['templates']['bash_script'] = {
+  'path' => '/usr/local/sbin/solr-undertow',
+  'cookbook' => 'scratchpads',
+  'source' => 'solr-undertow.bash.erb',
+  'owner' => 'root',
+  'group' => 'root',
+  'mode' => '0755'
+}
 # Configuration file
-default['scratchpads']['solr-undertow']['templates']['conf_file']['path'] = '/var/lib/solr-undertow/solr-undertow.conf'
-default['scratchpads']['solr-undertow']['templates']['conf_file']['cookbook'] = 'scratchpads'
-default['scratchpads']['solr-undertow']['templates']['conf_file']['source'] = 'solr-undertow.conf.erb'
-default['scratchpads']['solr-undertow']['templates']['conf_file']['owner'] = 'root'
-default['scratchpads']['solr-undertow']['templates']['conf_file']['group'] = 'root'
-default['scratchpads']['solr-undertow']['templates']['conf_file']['mode'] = '0755'
+default['scratchpads']['solr-undertow']['templates']['conf_file'] = {
+  'path' => '/var/lib/solr-undertow/solr-undertow.conf',
+  'cookbook' => 'scratchpads',
+  'source' => 'solr-undertow.conf.erb',
+  'owner' => 'root',
+  'group' => 'root',
+  'mode' => '0755'
+}
 # Configuration xml file
-default['scratchpads']['solr-undertow']['templates']['conf_xml']['path'] = '/var/lib/solr-undertow/solr-home/solr.xml'
-default['scratchpads']['solr-undertow']['templates']['conf_xml']['cookbook'] = 'scratchpads'
-default['scratchpads']['solr-undertow']['templates']['conf_xml']['source'] = 'solr.xml.erb'
-default['scratchpads']['solr-undertow']['templates']['conf_xml']['owner'] = 'root'
-default['scratchpads']['solr-undertow']['templates']['conf_xml']['group'] = 'root'
-default['scratchpads']['solr-undertow']['templates']['conf_xml']['mode'] = '0755'
+default['scratchpads']['solr-undertow']['templates']['conf_xml'] = {
+  'path' => '/var/lib/solr-undertow/solr-home/solr.xml',
+  'cookbook' => 'scratchpads',
+  'source' => 'solr.xml.erb',
+  'owner' => 'root',
+  'group' => 'root',
+  'mode' => '0755'
+}
 # Configuration zoo file
-default['scratchpads']['solr-undertow']['templates']['cfg_file']['path'] = '/var/lib/solr-undertow/solr-home/zoo.cfg'
-default['scratchpads']['solr-undertow']['templates']['cfg_file']['cookbook'] = 'scratchpads'
-default['scratchpads']['solr-undertow']['templates']['cfg_file']['source'] = 'zoo.cfg.erb'
-default['scratchpads']['solr-undertow']['templates']['cfg_file']['owner'] = 'root'
-default['scratchpads']['solr-undertow']['templates']['cfg_file']['group'] = 'root'
-default['scratchpads']['solr-undertow']['templates']['cfg_file']['mode'] = '0755'
+default['scratchpads']['solr-undertow']['templates']['cfg_file'] = {
+  'path' => '/var/lib/solr-undertow/solr-home/zoo.cfg',
+  'cookbook' => 'scratchpads',
+  'source' => 'zoo.cfg.erb',
+  'owner' => 'root',
+  'group' => 'root',
+  'mode' => '0755'
+}
 # Service file
-default['scratchpads']['solr-undertow']['templates']['systemd_service']['path'] = '/etc/systemd/system/solr-undertow.service'
-default['scratchpads']['solr-undertow']['templates']['systemd_service']['source'] = 'solr-undertow.service.erb'
-default['scratchpads']['solr-undertow']['templates']['systemd_service']['cookbook'] = 'scratchpads'
-default['scratchpads']['solr-undertow']['templates']['systemd_service']['owner'] = 'root'
-default['scratchpads']['solr-undertow']['templates']['systemd_service']['group'] = 'root'
-default['scratchpads']['solr-undertow']['templates']['systemd_service']['mode'] = '0644'
+default['scratchpads']['solr-undertow']['templates']['systemd_service'] = {
+  'path' => '/etc/systemd/system/solr-undertow.service',
+  'source' => 'solr-undertow.service.erb',
+  'cookbook' => 'scratchpads',
+  'owner' => 'root',
+  'group' => 'root',
+  'mode' => '0644'
+}
 # core properties file
-default['scratchpads']['solr-undertow']['templates']['core_properties']['path'] = '/var/lib/solr-undertow/solr-home/scratchpads2/core.properties'
-default['scratchpads']['solr-undertow']['templates']['core_properties']['source'] = 'core.properties.erb'
-default['scratchpads']['solr-undertow']['templates']['core_properties']['cookbook'] = 'scratchpads'
-default['scratchpads']['solr-undertow']['templates']['core_properties']['owner'] = 'solrundertow'
-default['scratchpads']['solr-undertow']['templates']['core_properties']['group'] = 'solrundertow'
-default['scratchpads']['solr-undertow']['templates']['core_properties']['mode'] = '0644'
+default['scratchpads']['solr-undertow']['templates']['core_properties'] = {
+  'path' => '/var/lib/solr-undertow/solr-home/scratchpads2/core.properties',
+  'source' => 'core.properties.erb',
+  'cookbook' => 'scratchpads',
+  'owner' => 'solrundertow',
+  'group' => 'solrundertow',
+  'mode' => '0644'
+}
 # Scratchpads configuration archive
-default['scratchpads']['solr-undertow']['scratchpads_conf']['path'] = '/var/lib/solr-undertow/solr-home/scratchpads2/conf.tar.gz'
-default['scratchpads']['solr-undertow']['scratchpads_conf']['source'] = 'conf.tar.gz'
-default['scratchpads']['solr-undertow']['scratchpads_conf']['cookbook'] = 'scratchpads'
-default['scratchpads']['solr-undertow']['scratchpads_conf']['owner'] = 'root'
-default['scratchpads']['solr-undertow']['scratchpads_conf']['group'] = 'root'
-default['scratchpads']['solr-undertow']['scratchpads_conf']['mode'] = '0644'
+default['scratchpads']['solr-undertow']['scratchpads_conf'] = {
+  'path' => '/var/lib/solr-undertow/solr-home/scratchpads2/conf.tar.gz',
+  'source' => 'conf.tar.gz',
+  'cookbook' => 'scratchpads',
+  'owner' => 'root',
+  'group' => 'root',
+  'mode' => '0644'
+}

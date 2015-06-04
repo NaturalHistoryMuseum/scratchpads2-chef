122c78c66cd820dc8bd6909bdef70d9989fc432e
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-06-04 12:57:45 +0100

    Missing an 's' from the command hostsfile_entry.

--- cookbooks/scratchpads/recipes/default.rb
+++ cookbooks/scratchpads/recipes/default.rb
@@ -9,7 +9,7 @@ hostsfile_entry '127.0.1.1' do
   action    :remove
 end
 node['scratchpads']['additional_hosts'].each do|hostname, ip|
-  hostfile_entry ip do
+  hostfiles_entry ip do
     hostname hostname
     unique true
   end

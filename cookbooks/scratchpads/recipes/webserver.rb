23478615f8a81da18c21aff9d82e47dacb3ae3e0
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-06-09 14:56:00 +0100

    Ooops, needed to overwrite the specific variable, and not the whole 'variables' hash.

--- cookbooks/scratchpads/recipes/webserver.rb
+++ cookbooks/scratchpads/recipes/webserver.rb
@@ -67,7 +67,7 @@ if(data_host)
   node.default['scratchpads']['webserver']['apache']['templates']['help.scratchpads.eu']['database']['host'] = data_host['fqdn']
 end
 if(data_hosts)
-  node.default['scratchpads']['webserver']['apache']['templates']['help.scratchpads.eu']['templates']['help.scratchpads.eu.LocalSettings.php']['variables'] = data_hosts
+  node.default['scratchpads']['webserver']['apache']['templates']['help.scratchpads.eu']['templates']['help.scratchpads.eu.LocalSettings.php']['variables']['sp_data_servers'] = data_hosts
 end
 
 # Add sites

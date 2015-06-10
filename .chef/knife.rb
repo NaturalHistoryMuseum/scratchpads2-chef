2acf1f4b0f89895f0f47078ad47a1078c739c08b
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-06-10 12:59:27 +0100

    Not sure if this needed changing, but I had issues validating with the Chef server.

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,12 +1,12 @@
-# See https://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
 
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                "simor"
-client_key               "#{current_dir}/user.pem"
-validation_client_name   "nhm"
-validation_key           "#{current_dir}/nhm.pem"
+client_key               "#{current_dir}/simor.pem"
+validation_client_name   "nhm-validator"
+validation_key           "#{current_dir}/nhm-validator.pem"
 chef_server_url          "https://chef.nhm.ac.uk/organizations/nhm"
 cookbook_path            ["#{current_dir}/../cookbooks"]
 

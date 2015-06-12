8f9ec27356004c6330b46157e5cf2214b0e8c218
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-06-12 11:49:58 +0100

    Improve readability of the file.

--- cookbooks/scratchpads/attributes/aegir.rb
+++ cookbooks/scratchpads/attributes/aegir.rb
@@ -17,8 +17,22 @@ default['scratchpads']['aegir']['comment'] = 'User which runs all of the behind 
 default['scratchpads']['aegir']['group'] = 'www-data'
 default['scratchpads']['aegir']['user'] = 'aegir'
 default['scratchpads']['aegir']['provision_version'] = 'provision-7.x-3.x'
-default['scratchpads']['aegir']['modules_to_install'] = ['hosting_queued','hosting_alias','hosting_clone','hosting_cron','hosting_migrate','hosting_signup','hosting_task_gc','hosting_web_pack','hosting_reinstall','hosting_auto_pack']
-default['scratchpads']['aegir']['modules_to_download'] = ['memcache','varnish']
+default['scratchpads']['aegir']['modules_to_install'] = [
+  'hosting_alias',
+  'hosting_auto_pack',
+  'hosting_clone',
+  'hosting_cron',
+  'hosting_migrate',
+  'hosting_queued',
+  'hosting_reinstall',
+  'hosting_signup',
+  'hosting_task_gc',
+  'hosting_web_pack'
+]
+default['scratchpads']['aegir']['modules_to_download'] = [
+  'memcache',
+  'varnish'
+]
 # Hosting Reinstall module settings for aegir
 default['scratchpads']['aegir']['hosting_reinstall']['repository'] = 'http://git.drupal.org/sandbox/ergonlogic/2386543.git'
 default['scratchpads']['aegir']['hosting_reinstall']['revision'] = '7.x-3.x'
@@ -31,9 +45,11 @@ default['scratchpads']['aegir']['scratchpads_master']['repository'] = 'https://g
 default['scratchpads']['aegir']['scratchpads_master']['checkout_branch'] = 'master'
 default['scratchpads']['aegir']['scratchpads_master']['timeout'] = 600
 # Files to create
-default['scratchpads']['aegir']['cookbook_files']['create-aegir-platform']['path'] = '/usr/local/sbin/create-aegir-platform'
-default['scratchpads']['aegir']['cookbook_files']['create-aegir-platform']['source'] = 'create-aegir-platform'
-default['scratchpads']['aegir']['cookbook_files']['create-aegir-platform']['cookbook'] = 'scratchpads'
-default['scratchpads']['aegir']['cookbook_files']['create-aegir-platform']['owner'] = 'root'
-default['scratchpads']['aegir']['cookbook_files']['create-aegir-platform']['group'] = 'root'
-default['scratchpads']['aegir']['cookbook_files']['create-aegir-platform']['mode'] = '0755'
+default['scratchpads']['aegir']['cookbook_files']['create-aegir-platform'] = {
+  'path' => '/usr/local/sbin/create-aegir-platform',
+  'source' => 'create-aegir-platform',
+  'cookbook' => 'scratchpads',
+  'owner' => 'root',
+  'group' => 'root',
+  'mode' => '0755'
+}

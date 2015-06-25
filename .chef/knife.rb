5b835198707f76a210ae412b68adc9af3c49966a
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-06-25 13:09:33 +0100

    Updated from chef server.

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,13 +1,13 @@
-# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+# See https://docs.chef.io/config_rb_knife.html for more information on knife configuration options
 
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "simor"
-client_key               "#{current_dir}/simor.pem"
+node_name                "sdrycroft"
+client_key               "#{current_dir}/sdrycroft.pem"
 validation_client_name   "nhm-validator"
 validation_key           "#{current_dir}/nhm-validator.pem"
-chef_server_url          "https://chef.nhm.ac.uk/organizations/nhm"
+chef_server_url          "https://api.opscode.com/organizations/nhm"
 cookbook_path            ["#{current_dir}/../cookbooks"]
 
 knife['editor'] = "nano"

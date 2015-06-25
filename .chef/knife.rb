e394732a3d086fe333e4c75f9713b11099237f92
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-06-25 16:47:34 +0100

    Reverted to using the local Chef server (because I stuffed up converting the encrypted data bags and needed to retrieve the originals).

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,13 +1,13 @@
-# See https://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
 
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "sdrycroft"
-client_key               "#{current_dir}/sdrycroft.pem"
+node_name                "simor"
+client_key               "#{current_dir}/simor.pem"
 validation_client_name   "nhm-validator"
 validation_key           "#{current_dir}/nhm-validator.pem"
-chef_server_url          "https://api.opscode.com/organizations/nhm"
+chef_server_url          "https://chef.nhm.ac.uk/organizations/nhm"
 cookbook_path            ["#{current_dir}/../cookbooks"]
 
 knife['editor'] = "nano"

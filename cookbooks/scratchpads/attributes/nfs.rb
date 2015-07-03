790883021c534a4007ff2baf52528d66d59c1e29
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-07-03 10:38:27 +0100

    Allow specifying hosts that can always connect.

--- cookbooks/scratchpads/attributes/nfs.rb
+++ cookbooks/scratchpads/attributes/nfs.rb
@@ -1,2 +1,4 @@
+# List of hosts to always allow to mount
+default['scratchpads']['nfs']['default_hosts'] = []
 # NFS
 default['scratchpads']['nfs']['exports'] = ['/var/www', '/var/aegir/platforms', '/var/aegir/backups', '/var/aegir/backups-databases']
--- cookbooks/scratchpads/recipes/nfs.rb
+++ cookbooks/scratchpads/recipes/nfs.rb
@@ -8,7 +8,8 @@
 if node['roles'].index(node['scratchpads']['control']['role']) then
   # Install NFS server and set it to allow access to certain servers.
   include_recipe 'nfs::server4'
-  client_hosts = []
+  # Add any hosts that you'd like to use for development work here.
+  client_hosts = node['scratchpads']['nfs']['default_hosts']
   unless Chef::Config[:solo]
     client_hosts_search = search(:node, "roles:#{node['scratchpads']['app']['role']} OR roles:#{node['scratchpads']['data']['role']}")
     client_hosts_search.each do|client_host|

e5e27c108f136047582d70c928b227470b4719e1
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-07-20 13:24:22 +0100

    Dir.foreach(...) does not work with wildcards (*).

--- cookbooks/scratchpads/recipes/postfix.rb
+++ cookbooks/scratchpads/recipes/postfix.rb
@@ -7,9 +7,15 @@
 # Only include the postfix recipe IF we are the control server
 if node['roles'].index(node['scratchpads']['control']['role']) then
   begin
-    Dir.foreach('/var/aegir/platforms/*/sites/*') do |domain|
-      default['postfix']['virtual_aliases']["@#{domain}"] = 'aegir'
-      default['postfix']['virtual_aliases_domains'][domain] = domain
+    Dir.foreach('/var/aegir/platforms') do |platform|
+      if File.exists?("/var/aegir/platforms/#{platform}/sites")
+        Dir.foreach("/var/aegir/platforms/#{platform}/sites") do |site|
+          if File.exists?("/var/aegir/platforms/#{platform}/sites/#{site}/settings.php")
+            node.default['postfix']['virtual_aliases']["@#{site}"] = 'aegir'
+            node.default['postfix']['virtual_aliases_domains'][site] = site
+          end
+        end
+      end
     end
   rescue
     Chef::Log.info('Not adding any domains to postfix configuration.')

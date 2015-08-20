99f588d52578d026a7ac22fc8ef3286b89ed82cf
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-08-20 15:48:45 +0100

    Only run a bootstrap if the previous one exited without error.

--- bootstrap.bash
+++ bootstrap.bash
@@ -1,8 +1,7 @@
 #!/bin/bash
-
-knife bootstrap sp-control-1.nhm.ac.uk -x simor -i ~/.ssh/id_rsa --sudo -r 'role[scratchpads-role-control]'
-knife bootstrap sp-app-1.nhm.ac.uk     -x simor -i ~/.ssh/id_rsa --sudo -r 'role[scratchpads-role-app]'
-knife bootstrap sp-app-2.nhm.ac.uk     -x simor -i ~/.ssh/id_rsa --sudo -r 'role[scratchpads-role-app]'
-knife bootstrap sp-data-1.nhm.ac.uk    -x simor -i ~/.ssh/id_rsa --sudo -r 'role[scratchpads-role-data]'
-knife bootstrap sp-data-2.nhm.ac.uk    -x simor -i ~/.ssh/id_rsa --sudo -r 'role[scratchpads-role-data]'
+knife bootstrap sp-control-1.nhm.ac.uk -x simor -i ~/.ssh/id_rsa --sudo -r 'role[scratchpads-role-control]' &&
+knife bootstrap sp-app-1.nhm.ac.uk     -x simor -i ~/.ssh/id_rsa --sudo -r 'role[scratchpads-role-app]'     &&
+knife bootstrap sp-app-2.nhm.ac.uk     -x simor -i ~/.ssh/id_rsa --sudo -r 'role[scratchpads-role-app]'     &&
+knife bootstrap sp-data-1.nhm.ac.uk    -x simor -i ~/.ssh/id_rsa --sudo -r 'role[scratchpads-role-data]'    &&
+knife bootstrap sp-data-2.nhm.ac.uk    -x simor -i ~/.ssh/id_rsa --sudo -r 'role[scratchpads-role-data]'    &&
 knife bootstrap sp-search-1.nhm.ac.uk  -x simor -i ~/.ssh/id_rsa --sudo -r 'role[scratchpads-role-search]'

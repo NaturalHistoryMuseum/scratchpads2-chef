f30e5ffd96572ed1cd922b9c9cc3fe22332a9ad2
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-10-05 17:37:17 +0100

    Add the pecl_http PECL module as required by the Taverna service.

--- cookbooks/scratchpads/attributes/webserver.rb
+++ cookbooks/scratchpads/attributes/webserver.rb
@@ -202,5 +202,6 @@ default['scratchpads']['webserver']['php']['pear']['pear_modules_custom_channels
 default['scratchpads']['webserver']['php']['pear']['pecl_modules'] = {
   'uploadprogress' => {'preferred_state' => 'stable'},
   'mailparse' => {'preferred_state' => 'stable'},
+  'pecl_http' => {'preferred_state' => 'stable'},
   'xhprof' => {'preferred_state' => 'beta'}
 }

ae39e3519b5d8e8b1db26511c2e89a8e3aed85b5
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-10-16 12:45:15 +0100

    Allow specific IP addresses to be blocked permanently. Also reinstate the connection limit from specific IP addresses - a single IP address should not open more than 20 connections to the server.

--- cookbooks/scratchpads/attributes/default.rb
+++ cookbooks/scratchpads/attributes/default.rb
@@ -1,5 +1,9 @@
 # Desktop machines that we will allow to SSH and access privileged ports on each machine
 default['scratchpads']['trusted_ip_addresses'] = ['157.140.126.0/24', '192.168.0.0/16', '157.140.121.33/32']
+# Permanently blocked IP addresses (rude Bots or spammers)
+default['scratchpads']['blocked_ip_addresses'] = [
+  '185.38.251.209'
+]
 # The following will be populated during a Chef run.
 default['scratchpads']['all_ip_addresses'] = []
 
